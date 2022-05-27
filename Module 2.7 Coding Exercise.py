fac7196ad28231b2345da525933f424bf72463ce
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-28 04:08:53 +0700

    Fourth Exercise

---
+++ Module 2.7 Coding Exercise.py
@@ -0,0 +1,6 @@
+name = input("Put your name here: ")
+age = (input ("Put your age here: "))
+favorite_color = input("Put your favorite color here: ")
+favorite_movie = input("Put your favorite movie here: ")
+mobile_number = int(input ("Put your mobile number here: "))
+motto_in_life = input("Put your motto in life here: ")
